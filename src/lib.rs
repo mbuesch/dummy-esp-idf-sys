c36973076f4cb33dca0b9c3d34250560ee6622f3
Michael Buesch <m@bues.ch>
2023-01-15 19:40:16 +0100

    Add esp_wifi_set_country_code and error macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
 // -*- coding: utf-8 -*-
 
+use std::ffi::{c_char, c_int};
+
+#[allow(non_camel_case_types)]
+pub type esp_err_t = c_int;
+
 #[derive(Debug)]
 pub struct EspError;
+
+impl EspError {
+    pub fn convert(_error: esp_err_t) -> Result<(), Self> {
+        Ok(())
+    }
+}
 
 impl std::error::Error for EspError {}
 
@@ -9,6 +20,20 @@ impl std::fmt::Display for EspError {
     fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         Ok(())
     }
+}
+
+pub unsafe fn esp_wifi_set_country_code(
+    _country: *const c_char,
+    _ieee80211d_enabled: bool,
+) -> esp_err_t {
+    0
+}
+
+#[macro_export]
+macro_rules! esp {
+    ($err:expr) => {{
+        $crate::EspError::convert($err as $crate::esp_err_t)
+    }};
 }
 
 // vim: ts=4 sw=4 expandtab
